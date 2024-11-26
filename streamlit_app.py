50bf5da2960f1635909836bc4f54ee47799dbc4d
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-26 16:00:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🎈 Jumar Buladaco Project')
 
-st.write('A machine learning app that categorizes penguins!')
+st.info('A machine learning app that categorizes penguins!')
