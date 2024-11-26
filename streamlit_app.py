d3940216c5586c6eb0ea07387782891e527404e9
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-26 16:16:12 +0800

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🎈 Jumar Buladaco Project')
 st.info('A machine learning app that categorizes penguins')
 
 with st.expander('Data'):
-  st.write('**Raw Data')
+  st.write('*Raw Data')
   df = pd.read_csv('https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/penguins_cleaned.csv')
   df
